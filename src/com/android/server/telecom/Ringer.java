9f66eae74b06c27d2afc86b640d7414eded50491
Alexey Derendyaev <alexxxdev@yandex.ru>
2018-05-02 10:33:20 +0700

    Added features flash-call-on-waiting [2/3]
    Added features flash-call-on-waiting [2/3]
    
    Derp

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -28,6 +28,7 @@ import android.os.Bundle;
 import android.os.Vibrator;
 
 import com.android.internal.annotations.VisibleForTesting;
+import import com.android.internal.util.du.Utils;
 
 /**
  * Controls the ringtone player.
@@ -85,6 +86,8 @@ public class Ringer {
      * Used to track the status of {@link #mVibrator} in the case of simultaneous incoming calls.
      */
     private boolean mIsVibrating = false;
+
+    private boolean mIsFlash = false;
 
     /** Initializes the Ringer. */
     @VisibleForTesting
@@ -167,8 +170,14 @@ public class Ringer {
             mVibratingCall = foregroundCall;
             mVibrator.vibrate(mVibrationEffect, VIBRATION_ATTRIBUTES);
             mIsVibrating = true;
+
         } else if (mIsVibrating) {
             Log.addEvent(foregroundCall, LogUtils.Events.SKIP_VIBRATION, "already vibrating");
+        }
+
+        if (!mIsFlash && Settings.System.getIntForUser(mContext.getContentResolver(),  Settings.System.FLASH_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1) {
+            Utils.toggleCameraFlashOn();
+            mIsFlash = true;
         }
 
         return shouldAcquireAudioFocus;
@@ -215,6 +224,11 @@ public class Ringer {
             mVibrator.cancel();
             mIsVibrating = false;
             mVibratingCall = null;
+
+        }
+        if (mIsFlash && Settings.System.getIntForUser(mContext.getContentResolver(), Settings.System.FLASH_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1) {
+            Utils.toggleCameraFlashOff();
+            mIsFlash = false;
         }
     }
 
